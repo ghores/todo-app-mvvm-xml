9624f1bb17b5633dc1bd3de816b3cdd15150d4ff
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-27 11:34:52 +0330

    implementation Extensions.kt

--- app/src/main/java/com/example/todoapp/ui/main/note/NoteFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/main/note/NoteFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentNoteBinding
+import com.example.todoapp.utils.setupListWithAdapter
+import com.example.todoapp.viewmodel.NoteViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +18,11 @@ class NoteFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding
 
+
+    //Other
+    private val viewModel: NoteViewModel by viewModels()
+    private var category = ""
+    private var priority = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +35,20 @@ class NoteFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         //InitViews
         binding?.apply {
-
+            //Spinner Category
+            viewModel.loadCategoriesData()
+            viewModel.categoriesList.observe(viewLifecycleOwner) {
+                categoriesSpinner.setupListWithAdapter(it) { itItem ->
+                    category = itItem
+                }
+            }
+            //Spinner Category
+            viewModel.loadPrioritiesData()
+            viewModel.prioritiesList.observe(viewLifecycleOwner) {
+                prioritySpinner.setupListWithAdapter(it) { itItem ->
+                    priority = itItem
+                }
+            }
         }
     }
 
--- app/src/main/java/com/example/todoapp/utils/Extensions.kt
+++ app/src/main/java/com/example/todoapp/utils/Extensions.kt
@@ -1,2 +1,21 @@
 package com.example.todoapp.utils
 
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+fun Spinner.setupListWithAdapter(list: MutableList<String>, callback: (String) -> Unit) {
+    val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, list)
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    this.adapter = adapter
+    this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            callback(list[p2])
+        }
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+
+        }
+    }
+}
